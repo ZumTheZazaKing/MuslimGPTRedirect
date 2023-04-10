141d43d7bbc46d8c277d929b8df76bcee3941e6e
ZumTheZazaKing <zahidi85543@gmail.com>
2023-04-10 10:39:33 +0800

    Now redirects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 function App() {
+
+  const goToNewURL = () => {
+    window.location.href = "https://muslimgpt.teknologikomputeransp1.com"
+  }
+
   return (
     <div className="App">
-      <iframe
-        src="https://zumthezazaking-tpgmilsumzum.hf.space"
-        title="MuslimGPT"
-      ></iframe>
+      <p className="desc">MuslimGPT has moved to a new URL:</p>
+      <br/>
+      <button onClick={() => goToNewURL()}>Go To New URL</button>
     </div>
   );
 }
